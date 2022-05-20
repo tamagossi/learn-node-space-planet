60f5634dacb00de20a564ac3f443332ccd45b017
Raka Pratama <mgf.prauliyatama@gmail.com>
2022-05-20 21:13:32 +0700

    feat(): add function to filter habitable planet

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 import fs from 'fs';
 import { parse } from 'csv-parse';
 
-const result = [];
+const habitablePlanet = [];
+
+const isHabitablePlanet = (planet) => planet['koi_disposition'] === 'CONFIRMED';
 
 fs.createReadStream('kepler_data.csv')
+	.pipe(
+		parse({
+			comment: '#',
+			columns: true,
+		})
+	)
 	.on('data', (data) => {
-		result.push(data);
+		if (isHabitablePlanet(data)) {
+			habitablePlanet.push(data);
+		}
 	})
 	.on('error', (err) => {
 		console.error(err);
 	})
 	.on('end', () => {
-		console.log(result);
+		console.log(habitablePlanet);
 		console.log('DONE');
 	});
-
-// parse();
