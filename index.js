15090e5c689197721e37e2e6869dc4e0b66b19af
Raka Pratama <mgf.prauliyatama@gmail.com>
2022-05-20 21:15:41 +0700

    feat(): add parameterv to filter habitable planet

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import { parse } from 'csv-parse';
 
 const habitablePlanet = [];
 
-const isHabitablePlanet = (planet) => planet['koi_disposition'] === 'CONFIRMED';
+const isHabitablePlanet = (planet) =>
+	planet['koi_disposition'] === 'CONFIRMED' &&
+	planet['koi_insol'] > 0.36 &&
+	planet['koi_insol'] < 1.11 &&
+	planet['koi_prad'] < 1.6;
 
 fs.createReadStream('kepler_data.csv')
 	.pipe(
